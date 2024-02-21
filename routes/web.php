ca3535b78aea2d5d7010fa645fe04a3ba02db85e
Alejandro <alejandro.dominguez-lopez@iesruizgijon.com>
2024-02-21 09:08:01 +0100

    Primer commit-Probando y creando Controllers y Routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +16,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', HomeController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('cursos', [CursoController::class, 'index']);
+
+Route::get('cursos/create', [CursoController::class, 'create']);
+
+Route::get('cursos/{curso}', [CursoController::class, 'show']);
+
+// Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+//     if ($categoria) {
+//         return "Bienvenido al curso $curso de la categoría $categoria";
+//     } else {
+//         return "Bienvenido al curso $curso";
+//     }
+// });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,4 +45,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
